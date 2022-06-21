6b51b0a71832117198501a9a628cffd661480bbc
Zachary <leeyihong03@gmail.com>
2022-06-21 14:46:18 -0400

    updateOffense implemented

--- backend/routers/offenseRouter.js
+++ backend/routers/offenseRouter.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { quickPermissionsCheck,authenticateUser } = require('../middleware/authentication');
-const {createOffense,getImages,uploadImage} = require('../controllers/offense')
+const {createOffense,getImages,uploadImage,updateOffense} = require('../controllers/offense')
 
 router.post('/createOffense', authenticateUser, quickPermissionsCheck('police'), createOffense) 
 router.post('/uploadImages', authenticateUser, quickPermissionsCheck('police'), uploadImage) 
 router.get('/getImages', authenticateUser, quickPermissionsCheck('police'), getImages)
+router.patch('/updateOffense/:id', authenticateUser, quickPermissionsCheck('admin'), updateOffense)
 
 module.exports = router
