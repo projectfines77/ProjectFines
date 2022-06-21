d1d8babf3bc86d3e4fab836a55d0d8341208025e
Zachary <leeyihong03@gmail.com>
2022-06-21 14:31:43 -0400

    Added link for offense to a car. Added error checking in getImages

--- backend/controllers/offense.js
+++ backend/controllers/offense.js
@@ -9,6 +9,9 @@ const fs = require('fs');
 const getImages = async (req,res) => {
     const {carPlate} = req.body
     const car = await Car.findOne({carPlate:carPlate})
+    if(!car){
+      throw new CustomErrors.NotFoundError(`Car plate ${carPlate} not found in database. Issue paper ticket.`)
+    }
     res.status(StatusCodes.OK).json(car.imagesOfThisCar)
 }
 
@@ -27,7 +30,10 @@ const uploadImage = async (req, res) => {
 
 const createOffense = async (req,res) => {
     const {offenseType, carPlate, imageEvidence, notes} = req.body
-    console.log(req.police);
+    const linkToCar = await Car.findOne({carPlate:carPlate})
+    if(!linkToCar){
+      throw new CustomErrors.NotFoundError(`Car plate ${carPlate} not found in database. Issue paper ticket.`)
+    }
     const police = await Police.findOne({_id:req.police.policeMongoID})
     const offense = await Offense.create({
       policeThatIssuedOffenseID: req.police.policeMongoID,
@@ -39,6 +45,8 @@ const createOffense = async (req,res) => {
       resolvedDateTime: null,
       notes: notes
     })
+    linkToCar.offensesIncurred.push(offense._id)
+    await linkToCar.save()
     res.status(StatusCodes.CREATED).json({receipt: offense})
 }
 
